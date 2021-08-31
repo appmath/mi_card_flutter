4dd4369e973a9c6cb2f0128c25130434254aa04e
appmath <azeez.kadhi@gmailcom>
2021-08-31 07:45:30 -0700

    Blank slate: ready for the section 44, Tapping into Widget Properties.

--- lib/main.dart
+++ lib/main.dart
@@ -12,39 +12,8 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Container(
-              width: 100,
-              color: Colors.red,
-              height: double.infinity,
-              child: Text('Container 1'),
-            ),
-            Container(
-                color: Colors.teal,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      width: 100,
-                      height: 100,
-                      color: Colors.white,
-                    ),
-                    Container(
-                      width: 100,
-                      height: 100,
-                      color: Colors.green,
-                    ),
-                  ],
-                )),
-            Container(
-              width: 100,
-              height: double.infinity,
-              color: Colors.blue,
-              child: Text('Container 3'),
-            ),
-          ],
+            child: Column(
+          children: [],
         )),
       ),
     );
